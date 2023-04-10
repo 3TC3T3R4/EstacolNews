c0cab67566dd4535edc72e85d8b21104af3e9c77
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-04-10 11:25:47 -0500

    Created More Test

--- EstacolNewsSql/EditorTest/EditorRepositoryTest.cs
+++ EstacolNewsSql/EditorTest/EditorRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.Core.Resource;
+using EstacolNews.Domain.Sql.Commands;
 using EstacolNews.Domain.Sql.Entities;
 using EstacolNews.UseCases.Sql.Gateway.Repositories.Commands.EditorCommands;
 using Moq;
@@ -42,6 +43,62 @@ namespace EstacolNewsSql.EditorTest
             Assert.Equal(editorsList, result);
 
         }
+
+
+        [Fact]
+        public async Task CreateEditorAsync()
+        {
+            //arrange
+            var newEditor = new Editor
+            {
+                id_user = "MongoDb",
+                complete_name = "Test",
+                phone = "21354",
+                estate = true
+
+            };
+            //var editorCreated = new InsertNewEditor
+            //{
+            //    id_user = "MongoDb",
+            //    complete_name = "Test",
+            //    phone = "21354",
+            //    estate = true
+
+            //};
+            _mockEditorRepository.Setup(x => x.InsertEditorAsync(newEditor)).ReturnsAsync(newEditor);
+            //act
+            var result = await _mockEditorRepository.Object.InsertEditorAsync(newEditor);
+            //assert
+            Assert.Equal(newEditor, result);
+        }
+
+
+
+        [Fact]
+        public async Task GetEditorByIdAsync()
+        {
+            //Arrange
+            var editor = new Editor
+            {
+                id_user = "MongoDb",
+                complete_name = "Test",
+                phone = "21354",
+                estate = true
+
+            };
+            //var editorsList = new List<Editor> { editor };
+            _mockEditorRepository.Setup(x => x.GetEditorByIdAsync(1)).ReturnsAsync(editor);
+
+            //Act
+            var result = await _mockEditorRepository.Object.GetEditorByIdAsync(1);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(editor, result);
+        }
+
+
+
 
 
 
